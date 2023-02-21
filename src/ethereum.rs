6b7c4e51f1ff8a185f93de9e1581c622d0056044
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-02-21 14:32:26 -0300

    feat: eth uncoupled

---
+++ src/ethereum.rs
@@ -0,0 +1,24 @@
+
+pub mod ethereum {
+    use std::str::FromStr;
+
+    use web3::{
+        Web3,
+        transports::WebSocket,
+        types::{U256, H160}
+    };
+
+
+    pub async fn get_eth_balance(wss: Web3<WebSocket>, account_address: &String ) -> () {
+         
+        let account = H160::from_str(account_address).unwrap();
+        let balance = wss.eth().balance(account, None).await.unwrap();
+    
+        let wei_conv: U256 = U256::exp10(18);
+
+        println!(
+            "ETH balance: {} ETH",
+            balance.checked_div(wei_conv).unwrap()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 pub mod infura;
+pub mod ethereum;
 
 use std::env;
-use std::str::FromStr;
 
-// TODO: fix web3 dependency imports
-use web3::types::{H160, U256};
-
-// import infura crate
 use infura::infura::connect_to_infura;
+use ethereum::ethereum::get_eth_balance;
+
 #[tokio::main]
 async fn main() -> web3::Result<()> {
   
     dotenv::dotenv().ok();
-    let ws_url = &env::var("INFURA_WSS").unwrap();
-    let web3s = connect_to_infura(ws_url).await?;
-    let account = H160::from_str(&env::var("ACCOUNT_ADDRESS").unwrap());
+    let infura_wss = &env::var("INFURA_WSS").unwrap();
+    let account_address = &env::var("ACCOUNT_ADDRESS").unwrap();
     
-    let balance = web3s.eth().balance(account.unwrap(), None).await?;
-    
-    println!("Wei balance of {:?}: {:?} wei", account.unwrap(), balance);
-
-    let wei_conv: U256 = U256::exp10(18);
-
-    println!(
-        "ETH balance of {:?}: {} ETH",
-        account.unwrap(),
-        balance.checked_div(wei_conv).unwrap()
-    );
-
+    let web3s = connect_to_infura(infura_wss).await?;
+    get_eth_balance(web3s, account_address).await;
     Ok(())
-    
 }
-
-
