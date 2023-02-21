28bf42791bc7248e1529173c7944274d7746c1c7
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-02-21 14:07:51 -0300

    add: initial draft

---
+++ src/infura.rs
@@ -0,0 +1,12 @@
+pub mod infura {
+    use web3::{
+        transports::WebSocket,
+        Web3,
+        Result
+    };
+
+    pub async fn connect_to_infura(ws_url: &String) -> Result<Web3<WebSocket>> {
+       let websocket = WebSocket::new(ws_url).await?;
+        Ok(web3::Web3::new(websocket))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+pub mod infura;
+
+use std::env;
+use std::str::FromStr;
+
+// TODO: fix web3 dependency imports
+use web3::types::{H160, U256};
+
+// import infura crate
+use infura::infura::connect_to_infura;
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+  
+    dotenv::dotenv().ok();
+    let ws_url = &env::var("INFURA_WSS").unwrap();
+    let web3s = connect_to_infura(ws_url).await?;
+    let account = H160::from_str(&env::var("ACCOUNT_ADDRESS").unwrap());
+    
+    let balance = web3s.eth().balance(account.unwrap(), None).await?;
+    
+    println!("Wei balance of {:?}: {:?} wei", account.unwrap(), balance);
+
+    let wei_conv: U256 = U256::exp10(18);
+
+    println!(
+        "ETH balance of {:?}: {} ETH",
+        account.unwrap(),
+        balance.checked_div(wei_conv).unwrap()
+    );
+
+    Ok(())
+    
 }
+
+
