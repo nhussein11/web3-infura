9f3e3adeb36decb0b51c780fef271b343ebbfec8
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-02-24 19:45:15 -0300

    add: get block number fn

--- src/ethereum.rs
+++ src/ethereum.rs
@@ -2,20 +2,26 @@ pub mod ethereum {
     use std::str::FromStr;
 
     use web3::{
-        Transport,
         types::{H160, U256},
-        Web3,
+        Transport, Web3,
     };
 
     pub async fn get_eth_balance<T: Transport>(transport: Web3<T>, account_address: &String) -> () {
         let account: H160 = H160::from_str(account_address).unwrap();
+
         let balance = transport.eth().balance(account, None).await.unwrap();
 
         let wei_conv: U256 = U256::exp10(18);
-
         println!(
             "ETH balance: {} ETH",
             balance.checked_div(wei_conv).unwrap()
         );
     }
+
+    pub async fn get_eth_blocknumber<T: Transport>(transport: Web3<T>) -> () {
+        let block_number = transport.eth().block_number().await.unwrap();
+        println!("ETH block number: {}", block_number);
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod infura;
 
 use std::env;
 
-use ethereum::ethereum::get_eth_balance;
+use ethereum::ethereum::{get_eth_balance, get_eth_blocknumber};
 use infura::infura::{connect_to_infura_by_websocket, connect_to_infura_by_http};
 
 #[tokio::main]
@@ -12,13 +12,13 @@ async fn main() -> web3::Result<()> {
     let api_key = &env::var("INFURA_API_KEY").unwrap();
     let account_address = &env::var("ACCOUNT_ADDRESS").unwrap();
 
-    let celo_http = format!("https://celo-alfajores.infura.io/v3/{}", api_key);
-    // let infura_wss = format!("wss://mainnet.infura.io/ws/v3/{}", api_key);
+    // let celo_http = format!("https://celo-alfajores.infura.io/v3/{}", api_key);
+    let infura_wss = format!("wss://mainnet.infura.io/ws/v3/{}", api_key);
 
-    // let web3s = connect_to_infura_by_websocket(&infura_wss).await?;
+    let web3s = connect_to_infura_by_websocket(&infura_wss).await?;
 
-
-    let web3s = connect_to_infura_by_http(&celo_http).await?;
-    get_eth_balance(web3s, account_address).await;
+    // let web3s = connect_to_infura_by_http(&celo_http).await?;
+    get_eth_balance(web3s.clone(), account_address).await;
+    get_eth_blocknumber(web3s).await;
     Ok(())
 }
