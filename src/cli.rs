4efc21fa3c146da301e05ad220f550d1cde6f9e7
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-01 11:23:21 -0300

    fix: vbles names fixed

--- src/cli.rs
+++ src/cli.rs
@@ -8,19 +8,19 @@ pub mod cli {
     use infura::infura::{HttpBuilder, WebSocketBuilder};
 
     use clap::Parser;
-    use web3::{transports, Transport, Web3};
+    use web3::{transports, Transport as Web3Transport, Web3};
 
     #[derive(Parser, Debug)]
     #[clap(version = "1.0", author = "Author Name")]
     pub struct Commands {
         #[clap(value_enum, default_value = "http")]
-        pub transport: MyTransport,
+        pub transport: Transport,
 
         #[clap(subcommand)]
         pub ethereum_subcommands: EthereumSubcommands,
     }
     #[derive(clap::ValueEnum, Debug, Clone, PartialEq)]
-    pub enum MyTransport {
+    pub enum Transport {
         Http = 1,
         WebSocket = 2,
     }
@@ -45,12 +45,12 @@ pub mod cli {
         let args = Commands::parse();
 
         match args.transport {
-            MyTransport::Http => {
+            Transport::Http => {
                 let http_url = format!("https://mainnet.infura.io/v3/{}", api_key);
                 let web3s = HttpBuilder::new(http_url).build();
                 run_ethereum_subcommands(args.ethereum_subcommands, &web3s).await;
             }
-            MyTransport::WebSocket => {
+            Transport::WebSocket => {
                 let ws_url = format!("wss://mainnet.infura.io/ws/v3/{}", api_key);
                 let web3s = WebSocketBuilder::new(ws_url).build().await;
                 run_ethereum_subcommands(args.ethereum_subcommands, &web3s).await;
@@ -58,7 +58,7 @@ pub mod cli {
         }
     }
 
-    async fn run_ethereum_subcommands<T: Transport>(args: EthereumSubcommands, web3s: &Web3<T>) {
+    async fn run_ethereum_subcommands<T: Web3Transport>(args: EthereumSubcommands, web3s: &Web3<T>) {
         match args {
             EthereumSubcommands::Balance(balance) => {
                 get_eth_balance(&web3s, &balance.address).await;
