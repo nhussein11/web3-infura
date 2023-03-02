e18f02846005c29150037091c47de879741927d5
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-02 11:18:20 -0300

    fix: dbg get eth balance unit test

--- src/tests.rs
+++ src/tests.rs
@@ -11,11 +11,16 @@ mod tests {
         dotenv::dotenv().ok();
         let api_key = &env::var("INFURA_API_KEY").unwrap();
         let http_url = format!("{}{}", ETH_HTTP_URL, api_key);
-
+        println!("http_url: {}", http_url);
         let web3s = HttpBuilder::new(http_url).build();
         let eth_balance = get_eth_balance(&web3s, address).await;
 
         // The current balance of the address (at the time of writing this test) is 33 ETH
         assert_eq!(eth_balance, "33");
     }
+
+   #[tokio::test]
+   async fn try_to_get_ethereum_balance() {
+
+   }
 }
