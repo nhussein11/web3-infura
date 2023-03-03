999e3957d80277f704f64dd696f8ba72ec5f6302
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-03 00:23:33 -0300

    fix: all unit test solved

--- src/tests.rs
+++ src/tests.rs
@@ -4,7 +4,7 @@ mod tests {
     use web3::Error;
 
     use crate::ethereum::ethereum::{
-        get_eth_balance, get_eth_blocknumber, get_eth_gasprice, ETH_HTTP_URL,
+        get_eth_balance, get_eth_blocknumber, get_eth_gasprice, ETH_HTTP_URL, ETH_WS_URL,
     };
     use crate::infura::infura::{HttpBuilder, WebSocketBuilder};
     use std::env;
@@ -24,93 +24,16 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn try_to_get_ethereum_balance_with_invalid_address() {
-        let address = "invalid address";
-
-        dotenv::dotenv().ok();
-        let api_key = &env::var("INFURA_API_KEY").unwrap();
-        let http_url = format!("{}{}", ETH_HTTP_URL, api_key);
-        let web3s = HttpBuilder::new(http_url).build();
-
-        let eth_balance = get_eth_balance(&web3s, &address).await.unwrap_err();
-        let expected_error: Error =
-            Error::InvalidResponse(r#"Invalid account address"#.to_string());
-
-        assert!(matches!(eth_balance, expected_error));
-    }
-
-    #[tokio::test]
-    async fn try_to_get_ethereum_balance_with_invalid_url_http() {
-        let address = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F";
-
-        let http_url = "https://mainnet.infura.io/v3/invalid";
-        let web3s = HttpBuilder::new(http_url.to_string()).build();
-
-        let eth_balance = get_eth_balance(&web3s, &address).await.unwrap_err();
-
-        let transport_error_expected_code = TransportError::Code(401);
-        let expected_error: Error = Error::Transport(transport_error_expected_code);
-
-        assert!(matches!(expected_error, eth_balance)); // TODO: check this line
-    }
-
-    #[tokio::test]
-    async fn try_to_get_ethereum_balance_with_invalid_url_ws() {
-        let address = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F";
-
-        let ws_url = "wss://mainnet.infura.io/ws/v3/invalid";
-        let web3s = HttpBuilder::new(ws_url.to_string()).build();
-
-        let eth_balance = get_eth_balance(&web3s, &address).await.unwrap_err();
-
-        let transport_error_expected_code = TransportError::Code(401);
-        let expected_error: Error = Error::Transport(transport_error_expected_code);
-
-        assert!(matches!(expected_error, eth_balance)); // TODO: check this line
-    }
-
-    #[tokio::test]
     async fn get_ethereum_block_number() {
         dotenv::dotenv().ok();
         let api_key = &env::var("INFURA_API_KEY").unwrap();
         let http_url = format!("{}{}", ETH_HTTP_URL, api_key);
         let web3s = HttpBuilder::new(http_url).build();
 
-        let block_number = get_eth_blocknumber(&web3s).await.unwrap();
+       let block_number = get_eth_blocknumber(&web3s).await.unwrap();
         // The current block number (at the time of writing this test) is 16.734.004
         let block_number = block_number.replace(".", "");
         assert!(block_number.parse::<u64>().unwrap() > 16_000_000);
-    }
-
-    #[tokio::test]
-    async fn try_to_get_ethereum_block_number_with_invalid_http_url() {
-        let invalid_http_url = "https://mainnet.infura.io/v3/invalid".to_string();
-        let web3s = HttpBuilder::new(invalid_http_url).build();
-
-        let block_number = get_eth_blocknumber(&web3s).await.unwrap_err();
-        let transport_error_expected_code = TransportError::Code(401);
-        
-        match block_number {
-            Error::Transport(transport_error) => {
-                assert_eq!(transport_error_expected_code, transport_error);
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    #[tokio::test]
-    async fn try_to_get_ethereum_block_number_with_invalid_ws_url() {
-        let invalid_ws_url = "wss://mainnet.infura.io/ws/v3/invalid".to_string();
-        let web3s = WebSocketBuilder::new(invalid_ws_url).build().await.unwrap_err();
-
-        let transport_error_expected_code = TransportError::Code(401);
-        
-        match web3s {
-            Error::Transport(transport_error) => {
-                assert_eq!(transport_error_expected_code, transport_error);
-            }
-            _ => unreachable!(),
-        }
     }
 
     #[tokio::test]
@@ -128,32 +51,77 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn try_to_get_ethereum_gas_price_with_invalid_http_url() {
+    async fn try_to_build_connection_with_invalid_http_url() {
         let invalid_http_url = "https://mainnet.infura.io/v3/invalid".to_string();
         let web3s = HttpBuilder::new(invalid_http_url).build();
 
         let gas_price = get_eth_gasprice(&web3s).await.unwrap_err();
         let transport_error_expected_code = TransportError::Code(401);
-        
+
         match gas_price {
             Error::Transport(transport_error) => {
                 assert_eq!(transport_error_expected_code, transport_error);
             }
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 
     #[tokio::test]
-    async fn try_to_get_ethereum_gas_price_with_invalid_ws_url() {
+    async fn try_to_build_connection_with_invalid_ws_url() {
         let invalid_ws_url = "wss://mainnet.infura.io/ws/v3/invalid".to_string();
-        let web3s = WebSocketBuilder::new(invalid_ws_url).build().await.unwrap_err();
-        
+        let web3s = WebSocketBuilder::new(invalid_ws_url)
+            .build()
+            .await
+            .unwrap_err();
+
         let transport_error_expected_code = TransportError::Code(401);
         match web3s {
             Error::Transport(transport_error) => {
                 assert_eq!(transport_error, transport_error_expected_code);
             }
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
+
+
+    #[tokio::test]
+    async fn try_to_get_ethereum_balance_with_invalid_address_with_http() {
+        let address = "invalid address";
+
+        dotenv::dotenv().ok();
+        let api_key = &env::var("INFURA_API_KEY").unwrap();
+        let http_url = format!("{}{}", ETH_HTTP_URL, api_key);
+        let web3s = HttpBuilder::new(http_url).build();
+
+        let eth_balance = get_eth_balance(&web3s, &address).await.unwrap_err();
+        let expected_error_message: String = "Invalid account address".to_string();
+
+        match eth_balance {
+            Error::InvalidResponse(error) => {
+                assert_eq!(error, expected_error_message);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    #[tokio::test]
+    async fn try_to_get_ethereum_balance_with_invalid_address_with_wss(){
+        let address = "invalid address";
+
+        dotenv::dotenv().ok();
+        let api_key = &env::var("INFURA_API_KEY").unwrap();
+        let ws_url = format!("{}{}", ETH_WS_URL, api_key);
+        let web3s = WebSocketBuilder::new(ws_url).build().await.unwrap();
+
+        let eth_balance = get_eth_balance(&web3s, &address).await.unwrap_err();
+        let expected_error_message: String = "Invalid account address".to_string();
+
+        match eth_balance {
+            Error::InvalidResponse(error) => {
+                assert_eq!(error, expected_error_message);
+            }
+            _ => unreachable!(),
+        }
+    }
+
 }
